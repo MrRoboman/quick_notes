47b12d5aaa1f49e45c7fc4f50cee8eb959dd3d21
Rob Kayson <rkayson@gmail.com>
2021-02-17 13:46:03 -0800

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+    note = Note.create(body: Faker::ChuckNorris.fact)
+end
+
+Tag.create(name: 'Chuck Norris')
